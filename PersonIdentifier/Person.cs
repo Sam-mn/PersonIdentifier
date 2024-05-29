73a925ca63f7ca15189d7da46b271639c187cce3
samer <samer@SAMER>
2024-05-29 23:37:33 +0200

    Add project files.

---
+++ PersonIdentifier/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonIdentifier
+{
+    internal class Person
+    {
+        public int aga {  get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+    }
+}
